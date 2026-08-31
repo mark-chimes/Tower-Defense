11224ce7f47f591308a817932424e7a51e17e0d8
Mark Chimes <chimes.mark@gmail.com>
2026-08-31 19:36:45 +0200

    Add visualization gizmo to CellController

--- Assets/Code/Scripts/CellController.cs
+++ Assets/Code/Scripts/CellController.cs
@@ -5,7 +5,7 @@ public class CellController : MonoBehaviour
     [SerializeField] private CellView cellPrefab;
     [SerializeField] private int width = 9;
     [SerializeField] private int height = 9;
-    [SerializeField] private float cellSize_meters = 10f;
+    [SerializeField] private float cellSizeMeters = 10f;
 
     private GridCell[,] cells;
 
@@ -23,19 +23,42 @@ public class CellController : MonoBehaviour
         {
             for (int z = 0; z < height; z++)
             {
-
                 GridCell cell = new GridCell(x,z);
-                float worldX = (x - (width-1) / 2f) * cellSize_meters;
-                float worldZ = (z - (height-1) / 2f) * cellSize_meters;
-                Vector3 pos = new Vector3(worldX, 0f, worldZ);
+                cells[x,z] = cell;
+
+                Vector3 pos = CoordsCellToWorld(x,z);
+
                 CellView view = Instantiate(cellPrefab, transform); 
-                view.Initialize(x, z);
                 view.transform.localPosition = pos;
                 view.name = $"Cell_{x}_{z}";
+                view.Initialize(x, z);
 
             }
         }
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = Color.grey;
+        Vector3 size = new Vector3(cellSizeMeters, 0f, cellSizeMeters);
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Gizmos.DrawWireCube(CoordsCellToWorld(x, z), size);
+            }
+        }
+    }
+
+    // Translate from cell coordinates to world coordinates
+    private Vector3 CoordsCellToWorld(int x, int z)
+    {
+        float worldX = (x - (width-1) / 2f) * cellSizeMeters;
+        float worldZ = (z - (height-1) / 2f) * cellSizeMeters;
+        return new Vector3(worldX, 0f, worldZ);
+    }
+
     // Update is called once per frame
     void Update()
     {
