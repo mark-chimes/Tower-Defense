f6f61de3ea2b62427ab91ef33bbcd5a1209a7fdc
Mark Chimes <chimes.mark@gmail.com>
2026-08-31 20:08:50 +0200

    New CellCoord type

---
+++ Assets/Code/Scripts/CellCoord.cs
@@ -0,0 +1,28 @@
+using System;
+
+public readonly struct CellCoord : IEquatable<CellCoord>
+{
+    public int X {get;}
+    public int Z {get;}
+
+    public CellCoord(int x, int z)
+    {
+        X = x;
+        Z = z;
+    }
+
+
+    
+    public override int GetHashCode() => HashCode.Combine(X,Z);
+    public override string ToString() => $"({X}, {Z})";
+
+    public bool Equals(CellCoord other) => X == other.X && Z == other.Z;
+    public override bool Equals(Object obj) => obj is CellCoord c && Equals(c);
+
+    public static bool operator ==(CellCoord a, CellCoord b) => a.Equals(b);
+    public static bool operator !=(CellCoord a, CellCoord b) => !a.Equals(b);
+
+
+    public string ToStringNumbersOnly() => $"{X} {Z}";
+
+}
