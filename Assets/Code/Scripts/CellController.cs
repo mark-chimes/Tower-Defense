8f795ad0724e5c869f667ef29e8e0d60486dba50
Mark Chimes <chimes.mark@gmail.com>
2026-09-01 12:17:35 +0200

    Prototype wall objects

--- Assets/Code/Scripts/CellController.cs
+++ Assets/Code/Scripts/CellController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CellController : MonoBehaviour
@@ -6,6 +7,10 @@ public class CellController : MonoBehaviour
     [SerializeField] private int width = 9;
     [SerializeField] private int height = 9;
     [SerializeField] private float cellSizeMeters = 10f;
+
+    [SerializeField] private GameObject walls_parent;
+    [SerializeField] private Wall wallPrefab;
+
 
     private GridCell[,] cells;
     private CellView[,] views;
@@ -19,7 +24,16 @@ public class CellController : MonoBehaviour
     void GenerateGrid()
     {
         cells = new GridCell[width, height];
-            
+        views = new CellView[width, height];
+
+        /* Just to test, remove */
+        CellCoord[] wallsCoords = new CellCoord[3];
+        wallsCoords[0] = new CellCoord(5,5);
+        wallsCoords[1] = new CellCoord(6,6);
+        wallsCoords[2] = new CellCoord(2,3);
+        /* ^^^ */
+
+ 
         for (int x=0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
@@ -31,9 +45,29 @@ public class CellController : MonoBehaviour
                 CellView view = Instantiate(cellPrefab, transform); 
                 Vector3 pos = CoordsCellToWorld(coord);
                 view.transform.localPosition = pos;
-                view.name = $"Cell_{coord.X}_{coord.Z}";
+                view.name = $"Cell_{x}_{z}";
                 view.Initialize(coord);
                 views[x,z] = view;
+
+                /* Just to test, remove */
+                for (int i=0; i < 3; i++)
+                {
+                    CellCoord wall_coord = wallsCoords[i];
+                    if (wall_coord == coord)
+                    {
+                        cell.has_wall = true;
+                    }
+                } 
+                /* ^^^ */
+
+                if (cell.has_wall)
+                {
+                    Wall wall = Instantiate(wallPrefab, transform); 
+                    wall.transform.localPosition = pos;
+                    wall.name = $"Wall_{x}_{z}";
+                    // todo, put wall on walls_parent
+                }
+
             }
         }
     }
--- Assets/Code/Scripts/GridCell.cs
+++ Assets/Code/Scripts/GridCell.cs
@@ -2,8 +2,11 @@ public class GridCell
 {
     public readonly CellCoord Coord;
 
+    public bool has_wall;
+
     public GridCell(CellCoord coord) {
         this.Coord = coord;
+        has_wall = false;
     }
     
 }
