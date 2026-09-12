723d01701e30148c1ee60cfba9c6f1ea895f84f1
Mark Chimes <chimes.mark@gmail.com>
2026-09-12 11:39:46 +0200

    Move grid gizmo to own class

---
+++ Assets/Code/Scripts/GridGizmo.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public static class GridGizmo
+{
+    public static void Draw(GridAuthor gridAuthor, Transform transform)
+    {
+        Matrix4x4 originalMatrix = Gizmos.matrix;
+        Color originalColor = Gizmos.color;
+
+        GridLayout layout = gridAuthor.Layout;
+        int width = layout.Width;
+        int height = layout.Height;
+
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Vector3 size = new Vector3(GridLayout.CellSize, 1f, GridLayout.CellSize);
+
+        Coord spawnPos = gridAuthor.SpawnPos;
+        Coord goalPos = gridAuthor.GoalPos;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Gizmos.color = Color.grey;
+                Coord c = new Coord(x, z);
+                if (c == spawnPos)
+                {
+                    Gizmos.color = Color.lightBlue;
+                    Gizmos.DrawCube(layout.CoordsToWorld(c), size);
+                }
+                else if (c == goalPos)
+                {
+                    Gizmos.color = Color.yellow;
+                    Gizmos.DrawCube(layout.CoordsToWorld(c), size);
+                }
+                else
+                {
+                    Gizmos.DrawWireCube(layout.CoordsToWorld(c), size);
+                }
+
+
+            }
+        }
+        Gizmos.matrix = originalMatrix;
+        Gizmos.color = originalColor;
+    }
+}
