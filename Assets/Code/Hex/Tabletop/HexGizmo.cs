2d6a4b738a2d9472711301f6239c64f2eb709c81
Mark Chimes <chimes.mark@gmail.com>
2026-09-25 20:14:22 +0200

    Minor cleanup

--- Assets/Code/Hex/Tabletop/HexGizmo.cs
+++ Assets/Code/Hex/Tabletop/HexGizmo.cs
@@ -7,31 +7,14 @@ public static class HexGizmo
         Matrix4x4 originalMatrix = Gizmos.matrix;
         Color originalColor = Gizmos.color;
         Gizmos.matrix = transform.localToWorldMatrix;
-        
-        HexCoord coord0 = new HexCoord(0,0);
-        Vector3 pos0 = HexLayout.CoordsToWorld(coord0);
 
-        HexCoord[] neighbors = coord0.Neighbours();
-
-
-
-        // TODO draw num rings depending on hexAuthor
-
-        if (isWire)
+        foreach (HexCoord coord in HexCoord.AllWithinRings(hexAuthor.NumRings))
         {
-            Gizmos.DrawWireMesh(hexMesh, pos0);
-            foreach (HexCoord coord in HexCoord.AllWithinRings(hexAuthor.NumRings)) {
-                Vector3 pos = HexLayout.CoordsToWorld(coord);
+            Vector3 pos = HexLayout.CoordsToWorld(coord);
+            if (isWire)
                 Gizmos.DrawWireMesh(hexMesh, pos);
-            }
-        }
-        else
-        {
-            Gizmos.DrawMesh(hexMesh, pos0);
-            foreach (HexCoord coord in HexCoord.AllWithinRings(hexAuthor.NumRings)) {
-                Vector3 pos = HexLayout.CoordsToWorld(coord);
+            else
                 Gizmos.DrawMesh(hexMesh, pos);
-            }
         }
 
         Gizmos.matrix = originalMatrix;
