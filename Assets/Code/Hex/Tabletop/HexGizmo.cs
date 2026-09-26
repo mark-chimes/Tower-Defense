ec9f488842dbc3b4c9ab55b367473a546b4bc944
Mark Chimes <chimes.mark@gmail.com>
2026-09-26 09:42:53 +0200

    Color mode cleanup

--- Assets/Code/Hex/Tabletop/HexGizmo.cs
+++ Assets/Code/Hex/Tabletop/HexGizmo.cs
@@ -20,9 +20,7 @@ public static class HexGizmo
         Color originalColor = Gizmos.color;
         Gizmos.matrix = transform.localToWorldMatrix;
 
-        int N = hexAuthor.NumRings;
-
-        foreach (HexCoord coord in HexCoord.AllWithinRings(N))
+        foreach (HexCoord coord in HexCoord.AllWithinRings(hexAuthor.NumRings))
         {
             Vector3 pos = HexLayout.CoordsToWorld(coord);
             Gizmos.color = ColorFor(coord, hexAuthor.NumRings, colorMode);
@@ -45,17 +43,17 @@ public static class HexGizmo
         _ => Color.grey,
     };
 
-    private static Color ColorOnZeroes(HexCoord coord)
+    private static Color ColorOnZeroes(HexCoord c)
     {
         // Center
-        if (coord.Q == 0 && coord.R == 0)
+        if (c.Q == 0 && c.R == 0)
             return Color.yellowNice;
 
-        if (coord.Q == 0)
+        if (c.Q == 0)
             return Color.green;
-        if (coord.R == 0)
+        if (c.R == 0)
             return Color.blue;
-        if (coord.S == 0)
+        if (c.S == 0)
             return Color.magenta;
 
         return Color.grey;
@@ -75,56 +73,40 @@ public static class HexGizmo
     ** South        Yellow
     ** South-East   Tennis-ball green
     **/
-    private static Color ColorRgb(HexCoord coord, int NumRings)
+    private static Color ColorRgb(HexCoord c, int N)
     {
-        int N = NumRings;
-        float tQ = Mathf.InverseLerp(-N, N, coord.Q);
-        float tS = Mathf.InverseLerp(-N, N, coord.S);
-        float tR = Mathf.InverseLerp(-N, N, coord.R);
+        float tQ = Mathf.InverseLerp(-N, N, c.Q);
+        float tS = Mathf.InverseLerp(-N, N, c.S);
+        float tR = Mathf.InverseLerp(-N, N, c.R);
 
         return new Color(tS, tQ, tR);
     }
 
     // Note the optional `Color?`
-    private static Color? ColorDiagonals(HexCoord coord, int NumRings)
+    private static Color? ColorDiagonals(HexCoord c, int N)
     {
         // Center
-        if (coord.Q == 0 && coord.R == 0)
+        if (c.Q == 0 && c.R == 0)
             return Color.yellowNice;
 
-        int N = NumRings;
-
-        float r = 0.5f;
-        float g = 0.5f;
-        float b = 0.5f;
-
-        if (coord.Q == coord.R)
+        if (c.Q == c.R)
         {
-            float t = Mathf.InverseLerp(N, -N, coord.S) / 2f;
+            float t = Mathf.InverseLerp(N, -N, c.S) / 2f;
             // diagonal goes from pure red at max S to pink
-            r = 1.0f;
-            g = t;
-            b = t;
-            return new Color(r, g, b);
+            return new Color(1.0f, t, t);
         }
-        if (coord.R == coord.S)
+        if (c.R == c.S)
         {
-            float t = Mathf.InverseLerp(N, -N, coord.Q) / 2f;
+            float t = Mathf.InverseLerp(N, -N, c.Q) / 2f;
             // diagonal goes from pure green at max Q to light green
-            r = t;
-            g = 1.0f;
-            b = t;
-            return new Color(r, g, b);
+            return new Color(t, 1.0f, t);
 
         }
-        if (coord.Q == coord.S)
+        if (c.Q == c.S)
         {
-            float t = Mathf.InverseLerp(N, -N, coord.R) / 2f;
+            float t = Mathf.InverseLerp(N, -N, c.R) / 2f;
             // diagonal goes from pure blue at max R to light blue
-            r = t;
-            g = t;
-            b = 1.0f;
-            return new Color(r, g, b);
+            return new Color(t, t, 1.0f);
         }
         return null;
     }
